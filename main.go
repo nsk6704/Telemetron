1136da9ad84a4954a3569aa8a3ee0e9fc2d3af43
Saketh Kashyap Nagendra <nsakethk2004@gmail.com>
2026-02-06 12:12:00 +0530

    feat: add basic HTTP server on port 8080

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello from Telemetron")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello from Telemetron")
+	})
+
+	log.Println("Server running on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
